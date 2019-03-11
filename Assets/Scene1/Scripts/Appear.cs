38e6cf2c418f10b416b54fa8aa156cedca2c4949
Matthew Brown <brownm18@rpi.edu>
2019-03-11 17:15:28 -0400

    Upload

---
+++ Assets/Scene1/Scripts/Appear.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Boo.Lang;
+
+public class Appear : MonoBehaviour {
+
+    //public Image img;
+    public GameObject img1;
+    public Canvas canvas;
+    private List<GameObject> images = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+        for (int i = 0; i < 5; i++){
+            GameObject temp = Instantiate(img1);
+            temp.transform.position = new Vector2(i*50, i*50);
+            temp.transform.SetParent(canvas.transform);
+            images.Add(temp);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("i")){
+            for (int i = 0; i < images.Count; i++){
+                Destroy(images[i]);
+            }
+            Debug.Log(images.Count);
+            images.Clear();
+            Debug.Log(images.Count);
+        }
+    }
+}
---
+++ Assets/Scene1/Scripts/HelloClient.cs
@@ -0,0 +1,60 @@
+﻿using AsyncIO;
+using NetMQ;
+using NetMQ.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+using Boo.Lang;
+
+public class HelloClient : MonoBehaviour{
+
+    public GameObject img;
+    private List<GameObject> images = new List<GameObject>();
+    public Canvas canvas;
+    private RequestSocket client;
+
+    private void Start(){
+        ForceDotNet.Force(); // this line is needed to prevent unity freeze after one use
+        client = new RequestSocket();
+        client.Connect("tcp://localhost:5555");
+    }
+
+    private void Update(){
+
+        for (int i = 0; i < images.Count; i++)
+        {
+            Destroy(images[i]);
+        }
+        images.Clear();
+
+        client.SendFrame("num_keypoints");
+        int num_keypoints = Int32.Parse(client.ReceiveFrameString());
+
+        for (int i = 0; i < num_keypoints; i++) { 
+            client.SendFrame("Loc_x");
+            string Loc_x = client.ReceiveFrameString();
+
+            client.SendFrame("Loc_y");
+            string Loc_y = client.ReceiveFrameString();
+
+            Debug.Log("Loc_y: " + Loc_y + " Loc_x: " + Loc_x);
+
+            int Pos_x = Int32.Parse(Loc_x);
+            int Pos_y = Int32.Parse(Loc_y);
+
+            GameObject temp = Instantiate(img);
+            temp.transform.position = new Vector2(Pos_x, Pos_y);
+            temp.transform.SetParent(canvas.transform);
+            images.Add(temp);
+
+        }
+    }
+
+    private void OnDestroy(){
+        client.SendFrame("END");
+        client.Disconnect("tcp://localhost:5555");
+        client.Dispose();
+        NetMQConfig.Cleanup(); // this line is needed to prevent unity freeze after one use
+    }
+}
